ec3039e10877a3ccd72b47e8d549ca3ade061491
DigiImmi <154073139+DigiImmi@users.noreply.github.com>
2024-04-30 19:22:11 -0700

    Add files via upload

---
+++ GeoGebra/navigation.js
@@ -0,0 +1,22 @@
+function loadNav(){
+	insertStyle("https://fonts.googleapis.com/icon?family=Material+Icons");
+}
+
+function insertStyle(url){
+	var styleEl = document.createElement('link');
+	styleEl.setAttribute("href",url);
+	styleEl.setAttribute("type","text/css");
+	styleEl.setAttribute("rel","stylesheet");
+	document.getElementsByTagName("head")[0].appendChild(styleEl);
+}
+
+insertStyle("navigation.css");
+
+
+    if (window.addEventListener){
+        window.addEventListener('load', loadNav, false);
+	}
+    else if (window.attachEvent){
+        window.attachEvent('onload', loadNav);
+	}
+
